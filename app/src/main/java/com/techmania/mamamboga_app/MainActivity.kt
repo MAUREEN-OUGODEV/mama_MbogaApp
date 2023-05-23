a123f2a9698b59740493b33f6e0634a0c9b48f26
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-05-23 15:50:24 +0300

    created splash page (introductory page of the app) and check page

--- app/src/main/java/com/techmania/mamamboga_app/MainActivity.kt
+++ app/src/main/java/com/techmania/mamamboga_app/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.techmania.mamamboga_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnStart: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        castview()
     }
+    override fun onResume() {
+        super.onResume()
+        btnStart.setOnClickListener {
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//
+//
+
+        }
+    }
+    fun castview(){
+        btnStart=findViewById(R.id.btnStart)
+    }
+
 }
+
